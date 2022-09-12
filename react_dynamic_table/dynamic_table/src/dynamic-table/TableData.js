8c81b6746ae7acbad9986a9dcd6a1a8db5809c97
JacquesR007 <100150332+JacquesR007@users.noreply.github.com>
2022-09-12 10:02:02 +0200

    Shopping_Cart

--- react_dynamic_table/dynamic_table/src/dynamic-table/TableData.js
+++ react_dynamic_table/dynamic_table/src/dynamic-table/TableData.js
@@ -1,9 +1,10 @@
 const TableData=[
-    {id:1, fullName:"Noor Khan", age:25, city:"Patna"},
-    {id:2, fullName:"Rapsan Jani", age:26, city:"Noida"},
-    {id:3, fullName:"Monika Singh", age:18, city:"New Delhi"},
-    {id:4, fullName:"Sunil Kumar", age:22, city: "Jaipur"},
-    {id:5, fullName:"Kajol Kumari", age: 21, city: "Chennai"}
+    {id:1, fullName:"Noor Khan", age:25, city:"Patna", email:"jack@gmai.com"},
+    {id:2, fullName:"Rapsan Jani", age:26, city:"Noida", email:"jack@gmai.com"},
+    {id:3, fullName:"Monika Singh", age:18, city:"New Delhi", email:"jack@gmai.com"},
+    {id:4, fullName:"Sunil Kumar", age:22, city: "Jaipur", email:"jack@gmai.com"},
+    {id:5, fullName:"Kajol Kumari", age: 21, city: "Chennai", email:"jack@gmai.com"},
+    {id:6, fullName:"Jack Heck", age: 33, city: "Pretoria", email:"jack@gmai.com"}
     
 ]
 export default TableData;
